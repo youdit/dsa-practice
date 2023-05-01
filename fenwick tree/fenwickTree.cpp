b6a31b76e7a2b79dcceb707e313be7a5c03a5fa8
Udit <udit.singh98@gmail.com>
2023-05-01 23:41:14 +0530

    made changes to the fenwick tree code

--- fenwick tree/fenwickTree.cpp
+++ fenwick tree/fenwickTree.cpp
@@ -3,25 +3,35 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-struct FenwickTree {
-    vector<int> bit;
-    int n;
+class FenwickTree {
 
-    FenwickTree(int n) {
-        this->n = n;
-        bit.assign(n, 0);
+private:
+    int n;
+    vector<int> T;
+
+public:
+
+    FenwickTree(int n){
+        this -> n = n;
+        T.assign(n, 0);
     }
 
-    FenwickTree(vector<int> const &a) : FenwickTree(a.size()) {
-        for(int i=0; i< a.size(); ++i) {
+    FenwickTree(vector<int> &a) : FenwickTree(a.size()) {
+        for(int i = 0; i < n; ++i) {
             add(i, a[i]);
+        }
+    }
+
+    void add(int idx, int delta) {
+        for(; idx < n; idx = idx | (idx + 1)) {
+            T[idx] += delta;
         }
     }
 
     int sum(int r) {
         int res = 0;
-        for(; r>=0 ; r = (r&r+1) - 1) {
-            res += bit[r];
+        for(; r >=0 ; r = (r&(r+1)) - 1) {
+            res += T[r];
         }
         return res;
     }
@@ -30,13 +40,16 @@ struct FenwickTree {
         return sum(r) - sum(l-1);
     }
 
-    void add(int idx, int delta) {
-        for(; idx < n; idx = idx | (idx + 1)) {
-            bit[idx] += delta;
-        }
+    vector<int> fenwickVector(){
+        return T;
     }
 };
 
-int main(){
 
+int main(){
+    vector<int> A = {1,2,3,4,5,6,7,8};
+    
+    FenwickTree tree = FenwickTree(A);
+    for(int x: tree.fenwickVector()) cout<<x<<" ";
+    cout<<"\n";
 }
